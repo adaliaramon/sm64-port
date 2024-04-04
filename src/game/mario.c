6ec362f41080ae12763040a25e4f0a9c3132dc28
Ramon Adàlia <adaliaramon@gmail.com>
2024-04-04 17:16:56 +0200

    Cheat: free BLJs

--- src/game/mario.c
+++ src/game/mario.c
@@ -874,6 +874,8 @@ static u32 set_mario_action_airborne(struct MarioState *m, u32 action, u32 actio
             //  too much velocity, but misses backwards longs allowing high negative speeds.
             if ((m->forwardVel *= 1.5f) > 48.0f) {
                 m->forwardVel = 48.0f;
+            } else if (m->forwardVel < 0.0f) {  // Cheat: free BLJs
+                m->vel[1] = 0.0f;
             }
             break;
 
